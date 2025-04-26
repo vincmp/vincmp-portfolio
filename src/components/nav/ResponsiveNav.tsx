6ec63b3013ce590e24956ea023af09705bacc82a
vincmp <vincmpimenta@proton.me>
2025-04-25 22:02:02 -0300

    008 - Fixed ResponsiveNav.tsx on Mobile Devices

--- src/components/nav/ResponsiveNav.tsx
+++ src/components/nav/ResponsiveNav.tsx
@@ -31,7 +31,7 @@ function MobileNavbar({
 }) {
   return (
     <div className="lg:hidden fixed top-0 left-0 right-0 bg-white border-b-2 border-[#E6E6E6] z-50 flex items-center justify-between px-6 py-2">
-      <div className="scale-[0.85] origin-left mb-1">
+      <div className="scale-[0.85] origin-left mt-4 mb-4">
         <DevProfile />
       </div>
       <button onClick={toggleMenu} className="focus:outline-none">
@@ -75,10 +75,12 @@ function MobileMenu({
       animate={{ height: "auto", opacity: 1 }}
       exit={{ height: 0, opacity: 1 }}
       transition={{ duration: 0.3 }}
-      className="lg:hidden fixed top-24 left-0 right-0 bg-white border-b-2 border-gray-200 z-40 overflow-hidden flex flex-col p-6 space-y-4"
+      className="lg:hidden fixed top-14 left-0 right-0 bg-white border-b-2 border-gray-200 z-40 overflow-hidden flex flex-col p-4 space-y-4"
     >
-      <div className="pt-4 space-y-[50px] pl-[25px] pb-[90px]">
-        <NavLinks pathname={pathname} setIsOpen={setIsOpen} />
+      <div className="pl-[25px] pb-[90px] pt-[56px]">
+        <div className="flex flex-col [&>*]:py-[24px]">
+          <NavLinks pathname={pathname} setIsOpen={setIsOpen} />
+        </div>
       </div>
       <div className="mt-auto pt-3 border-t-2 border-[#E6E6E6]">
         <div className="pt-3">
@@ -91,7 +93,7 @@ function MobileMenu({
 
 function DesktopSidebar({ pathname }: { pathname: string }) {
   return (
-    <aside className="hidden lg:flex lg:flex-col lg:fixed lg:top-0 lg:left-0 lg:h-full lg:w-80 bg-white border-r border-gray-200 p-6 z-40">
+    <aside className="hidden lg:flex lg:flex-col lg:fixed lg:top-0 lg:left-0 lg:h-full lg:w-80 bg-white border-r border-[#E6E6E6] p-6 z-40">
       <div className="mt-6">
         <DevProfile />
         <div className="border-b-2 border-[#E6E6E6] mt-12" />
