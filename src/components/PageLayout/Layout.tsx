f416ea31313093fe2c1b40ad74099535460d357a
vincmp <vincmpimenta@proton.me>
2025-04-26 21:30:12 -0300

    023 - Lol fixed responsive PageLayout with NavBar on mobile devices

--- src/components/PageLayout/Layout.tsx
+++ src/components/PageLayout/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <main className="min-h-screen bg-white pt-26 lg:pt-0 lg:pl-80">
+    <main className="min-h-screen bg-white pt-28 lg:pt-0 lg:pl-80">
       <ResponsiveNav />
       <div className="p-8 border-l border-[#E6E6E6]">{children}</div>
     </main>
