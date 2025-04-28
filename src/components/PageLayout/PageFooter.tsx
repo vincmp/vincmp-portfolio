c902bbe49526430afc541c0041125eed9a5a3687
vincmp <vincmpimenta@gmail.com>
2025-04-28 07:21:30 -0300

    026 - Fixed page footer on big screens.

--- src/components/PageLayout/PageFooter.tsx
+++ src/components/PageLayout/PageFooter.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const PageFooter: React.FC = () => {
   return (
-    <footer className="w-full text-white py-8 border-t-2 border-t-[#E6E6E6] max-w-6xl mx-auto flex items-center">
-      <div className="container mx-auto px-4">
+    <div className="w-full px-8">
+      <footer className="w-full text-white py-8 border-t-2 border-t-[#E6E6E6]">
         <div className="text-gray-400 text-sm py-[18px]">
           <p className="pagefooter-text">
             © Copyright {new Date().getFullYear()}. Todos os direitos
             reservados.
           </p>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 };
 
