27b8628c0d15f4ed78c621edc7b4cba16a34ffc8
vincmp <vincmpimenta@gmail.com>
2025-04-30 14:13:23 -0300

    037 - Fixed navbar border and text colors

--- src/components/PageLayout/PageFooter.tsx
+++ src/components/PageLayout/PageFooter.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageFooter: React.FC = () => {
   return (
     <div className="w-full px-8">
-      <footer className="w-full text-white py-8 border-t-2 border-t-[#E6E6E6]">
+      <footer className="w-full text-[var(--text-secondary)] py-8 border-t-2 border-[var(--border-color)]">
         <div className="text-gray-400 text-sm py-[18px]">
           <p className="pagefooter-text">
             © Copyright {new Date().getFullYear()}. Todos os direitos
